590f0744862836215d00734043074ba9c0fb928c
Gearsan <gearsan@MacBook-Pro-khxng-Gearsan.local>
2024-10-25 18:14:32 +0700

    add role decorators

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -5,6 +5,8 @@ import { CreateItemDto } from './dto/create-item.dto';
 import { UpdateItemDto } from './dto/update-item.dto';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { Role } from 'src/users/entities/user.entity';
 
 @Controller('items')
 export class ItemsController {
@@ -37,6 +39,7 @@ export class ItemsController {
   }
 
   @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles([Role.ADMIN, Role.MANAGER])
   @Patch(':id/approve')
   approve(@Param('id', ParseIntPipe) id: number) {
     return this.itemsService.approve(id);
